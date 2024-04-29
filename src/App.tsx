18e43837e9cffeeaa2fc14eea201c48b9c9505d2
emanuele <emanuele971@icloud.com>
2024-04-29 23:49:24 +0200

    effect text

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Model from "./Ocean1.tsx";
 import { Suspense } from "react";
 import Sound from "./sound";
 import Text from "./text";
+import { useState, useEffect } from "react";
 
 const getTimePreset = () => {
   const hour = new Date().getHours();
@@ -24,8 +25,23 @@ const getTimePreset = () => {
 const timePreset = getTimePreset();
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Tempo di caricamento del modello 3D
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
+      <div
+        className={`fixed z-50 w-screen h-screen transition-opacity duration-1000 bg-black flex items-center justify-center ${isLoading ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+      >
+        <div className="text-3xl text-white opacity-100">CHARGING..</div>
+      </div>
       <div className="w-screen h-screen overflow-hidden">
         {(timePreset === "forest" && <Sound className="text-white " />) || (
           <Sound />
